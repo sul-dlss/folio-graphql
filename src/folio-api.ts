567de87a78e1835d442e83162d02a17cfc99d9d9
Nick Budak <budak@stanford.edu>
2022-09-01 16:20:31 -0700

    Create a base FolioAPI class

--- src/folio-api.ts
+++ src/folio-api.ts
@@ -1,14 +1,34 @@
-// def get(path, **kwargs)
-// authenticated_request(path, method: :get, **kwargs)
-// end
-
-// def get_json(path, **kwargs)
-// parse(get(path, **kwargs))
-// end
-import { RESTDataSource } from '@apollo/datasource-rest';
+import config from "config"
+import { RESTDataSource, WillSendRequestOptions } from "@apollo/datasource-rest"
 
 export default class FolioAPI extends RESTDataSource {
+  override baseURL = config.get("folio.baseUrl") as string
 
+  // send these headers by default on every request
+  override willSendRequest(request: WillSendRequestOptions) {
+    if (this.token) request.headers["X-Okapi-Token"] = this.token
+    request.headers["X-Okapi-Tenant"] = "sul"
+    request.headers["User-Agent"] = "FolioApiClient"
+    request.headers["Accept"] = "application/json, text/plain"
+    request.headers["Content-Type"] = "application/json"
+  }
 
+  private token: string
+  private refreshToken: string
 
+  constructor(options = {}) {
+    super(options)
+
+    this.post("authn/login", {
+      body: {
+        username: config.get("folio.username"),
+        password: config.get("folio.password"),
+      }
+    })
+      .then(response => {
+        this.token = response.okapiToken
+        this.refreshToken = response.refreshToken
+      })
+      .catch(console.error)
+  }
 }
