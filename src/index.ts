08b1faf4f5e1eb078d7a88bda01dd7d016d4df7f
Nick Budak <budak@stanford.edu>
2022-09-01 17:05:05 -0700

    Rework entrypoint file to use server

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,35 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { readFileSync } from 'fs';
-import { RESTDataSource, WillSendRequestOptions } from '@apollo/datasource-rest';
-import { Patron } from './schema';
+import { ApolloServer } from "@apollo/server"
+import { startStandaloneServer } from "@apollo/server/standalone"
+import { readFileSync } from "fs"
+import PatronsAPI from "./patrons-api.js"
 
-// import { PatronsAPI } from './patrons-api';
-
-// const patrons = [
-//     {
-//         id: 1,
-//         name: 'Kate Chopin',
-//     },
-//     {
-//         id: 2,
-//         name: 'Paul Auster',
-//     },
-// ];
+const patrons = new PatronsAPI()
 
 // Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
-// const resolvers = {
-//     Query: {
-//         patrons: () => patrons,
-//     },
-// };
+const resolvers = {
+  Query: {
+    patron(parent, args, context, info) {
+      return patrons.getPatron(args.id)
+    }
+  },
+}
 
 // Read the schema.graphql into utf-8 string so we can pass it to Apollo
-const typeDefs = readFileSync('schema.graphql').toString('utf-8');
+const typeDefs = readFileSync("schema.graphql").toString("utf-8")
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
-    typeDefs
-});
+  typeDefs,
+  resolvers,
+})
 
 // Passing an ApolloServer instance to the `startStandaloneServer` function:
 //  1. creates an Express app
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
 })
 
 console.log(`🚀  Server ready at: ${url}`)
