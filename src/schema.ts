6e47f4b7166780c83112ce69b22a616e482472fc
Nick Budak <budak@stanford.edu>
2022-09-02 09:45:44 -0700

    Fill out schema for patron API

--- src/schema.ts
+++ src/schema.ts
@@ -10,12 +10,69 @@ export type Scalars = {
   Boolean: boolean;
   Int: number;
   Float: number;
+  Date: any;
+  UUID: any;
+};
+
+export type Charge = {
+  __typename?: 'Charge';
+  accrualDate?: Maybe<Scalars['Date']>;
+  chargeAmount?: Maybe<ChargeAmount>;
+  description?: Maybe<Scalars['String']>;
+  feeFineId?: Maybe<Scalars['UUID']>;
+  item?: Maybe<Item>;
+  reason?: Maybe<Scalars['String']>;
+  state?: Maybe<Scalars['String']>;
+};
+
+export type ChargeAmount = {
+  __typename?: 'ChargeAmount';
+  amount: Scalars['Float'];
+  isoCurrencyCode: Scalars['String'];
+};
+
+export type Hold = {
+  __typename?: 'Hold';
+  canceledByUserId?: Maybe<Scalars['UUID']>;
+  canceledDate?: Maybe<Scalars['String']>;
+  cancellationAdditionalInformation?: Maybe<Scalars['String']>;
+  cancellationReasonId?: Maybe<Scalars['UUID']>;
+  expirationDate?: Maybe<Scalars['Date']>;
+  item?: Maybe<Item>;
+  patronComments?: Maybe<Scalars['String']>;
+  pickupLocationId: Scalars['UUID'];
+  queuePosition?: Maybe<Scalars['Int']>;
+  requestDate: Scalars['Date'];
+  status?: Maybe<Scalars['String']>;
+};
+
+export type Item = {
+  __typename?: 'Item';
+  author?: Maybe<Scalars['String']>;
+  instanceId: Scalars['UUID'];
+  isbn?: Maybe<Scalars['String']>;
+  itemId?: Maybe<Scalars['UUID']>;
+  title?: Maybe<Scalars['String']>;
+};
+
+export type Loan = {
+  __typename?: 'Loan';
+  dueDate: Scalars['Date'];
+  item: Item;
+  loanDate: Scalars['Date'];
+  overdue: Scalars['Boolean'];
 };
 
 export type Patron = {
   __typename?: 'Patron';
-  id: Scalars['ID'];
-  name?: Maybe<Scalars['String']>;
+  charges: Array<Maybe<Charge>>;
+  holds: Array<Maybe<Hold>>;
+  id?: Maybe<Scalars['UUID']>;
+  loans: Array<Maybe<Loan>>;
+  totalCharges: ChargeAmount;
+  totalChargesCount: Scalars['Int'];
+  totalHolds: Scalars['Int'];
+  totalLoans: Scalars['Int'];
 };
 
 export type Query = {
@@ -25,5 +82,5 @@ export type Query = {
 
 
 export type QueryPatronArgs = {
-  id: Scalars['ID'];
+  id: Scalars['UUID'];
 };
