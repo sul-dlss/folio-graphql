ecb633c29b3c89f323cf374c5f469aa864a8bf90
Justin Coyne <jcoyne@justincoyne.com>
2023-05-17 12:32:03 -0500

    Add honeybadger

--- server.mjs
+++ server.mjs
@@ -8,6 +8,7 @@ import bodyParser from 'body-parser';
 import { resolvers } from './dist/index.js';
 
 import { readFileSync } from "fs"
+import Honeybadger from '@honeybadger-io/js';
 
 // Read the schema.graphql into utf-8 string so we can pass it to Apollo
 const typeDefs = readFileSync("schema.graphql").toString("utf-8")
@@ -24,11 +25,13 @@ const server = new ApolloServer({
 });
 await server.start();
 
+app.use(Honeybadger.requestHandler) // Use *before* all other app middleware.
 app.use(
   cors(),
   bodyParser.json(),
   expressMiddleware(server),
 );
+app.use(Honeybadger.errorHandler) // Use *after* all other app middleware
 
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
 console.log(`🚀 Server ready at http://localhost:4000`);
