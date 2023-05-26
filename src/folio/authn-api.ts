04d4d635709921dd333f863a902e7d00b81ea9fc
Chris Beer <chris@cbeer.info>
2023-05-26 09:07:12 -0700

    Extract AuthnAPI to its own file

---
+++ src/folio/authn-api.ts
@@ -0,0 +1,22 @@
+import { WillSendRequestOptions } from '@apollo/datasource-rest';
+import config from 'config';
+import OkapiAPI from "./okapi-api.js"
+
+export default class AuthnAPI extends OkapiAPI {
+  // send these headers by default on every request
+  override willSendRequest(request: WillSendRequestOptions) {
+    request.headers["X-Okapi-Tenant"] = config.get("folio.tenant")
+    request.headers["User-Agent"] = "FolioApiClient"
+    request.headers["Accept"] = "application/json, text/plain"
+    request.headers["Content-Type"] = "application/json"
+  }
+
+  async login(username, password) {
+    return this.post("authn/login", {
+      body: {
+        username,
+        password,
+      }
+    })
+  }
+}
